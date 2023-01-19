874e3a6472bf2473ce1ebbc5eaaf534ebfd68e89
ToshiroSama <alishdjuraev@gmail.com>
2023-01-19 13:20:19 +0500

    Initial Commit

---
+++ SupportTelecom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SupportTelecom
+//
+//  Created by Alisher Djuraev on 19/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
