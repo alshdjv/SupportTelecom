95fabeccabc23fd7ca5ba25b41484cddeb732f89
ToshiroSama <alishdjuraev@gmail.com>
2023-01-19 15:46:12 +0500

    initial commit. Colors model are added

---
+++ SupportTelecom/SupportViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+import SnapKit
+
+struct Colors {
+    static let primaryLight = UIColor(red: 43/255, green: 45/255, blue: 51/255, alpha: 1.0)
+    static let redLight = UIColor(red: 255/255, green: 59/255, blue: 48/255, alpha: 1.0)
+    static let secondaryLight = UIColor(red: 170/255, green: 171/255, blue: 173/255, alpha: 1.0)
+    static let primaryDark = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+    static let redDark = UIColor(red: 255/255, green: 69/255, blue: 58/255, alpha: 1.0)
+    static let secondaryDark = UIColor(red: 128/255, green: 129/255, blue: 133/255, alpha: 1.0)
+}
+
+final class SupportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
